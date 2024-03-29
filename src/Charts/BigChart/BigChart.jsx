772d093063b804fe62262455d3ca1ab28d052feb
Sai Mouli <141447420+SaiMouli3@users.noreply.github.com>
2024-03-29 22:24:37 +0530

    Update BigChart.jsx

--- src/Charts/BigChart/BigChart.jsx
+++ src/Charts/BigChart/BigChart.jsx
@@ -33,7 +33,7 @@ const BigChart = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://127.0.0.1:8000/webapp/shift/date");
+        const response = await axios.get("https://techno.pythonanywhere.com/webapp/shift/date");
         console.log("Fetched data:", response.data);
         setPerformanceData(response.data);
       } catch (error) {
