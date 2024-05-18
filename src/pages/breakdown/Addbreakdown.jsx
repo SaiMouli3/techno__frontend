d483a1327578e7857b70e6923f503c88a031f25b
Sunitha <sunithab188@gmail.com>
2024-05-18 10:16:07 +0530

    Addbreakdown,jsx

--- src/pages/breakdown/Addbreakdown.jsx
+++ src/pages/breakdown/Addbreakdown.jsx
@@ -6,36 +6,39 @@ import {
   DialogContent,
   DialogTitle,
   TextField,
-  MenuItem,
 } from "@mui/material";
-import Select from 'react-select'
+import Select from 'react-select';
 import axios from "axios";
 
 const AddBreakdown = ({ open, handleClose, handleAddBreakdown }) => {
   const [date, setDate] = useState(new Date().toISOString().split('T')[0]);
-  const [emp_ssn, setEmpSSN] = useState("");
+  const [empSSN, setEmpSSN] = useState("");
   const [toolCode, setToolCode] = useState("");
   const [machineId, setMachineId] = useState("");
-  const [lengthUsed, setLengthUsed] = useState("");
-  const [expectedLengthRemaining, setExpectedLengthRemaining] = useState("");
   const [replacedBy, setReplacedBy] = useState("");
   const [reason, setReason] = useState(" ");
   const [changeTime, setChangeTime] = useState(0);
   const [noOfMinIntoShift, setNoOfMinIntoShift] = useState("");
   const [toolOptions, setToolOptions] = useState([]);
   const [machineOptions, setMachineOptions] = useState([]);
-  
-   const [employeeData, setEmployeeData] = useState([]);
+  const [employeeData, setEmployeeData] = useState([]);
+  const [toolss, setToolss] = useState(null);
+  const [replacedTools, setReplacedTools] = useState([]);
+  const [filteredToolOptions, setFilteredToolOptions] = useState([]);
+
   const fetchData = async () => {
     try {
       const response = await axios.get(`${process.env.REACT_APP_URL}/webapp/api/employees/`);
       setEmployeeData(response.data);
     } catch (error) {
+      console.error("Error fetching employee data:", error);
     }
   };
-  useEffect(()=> {
+
+  useEffect(() => {
     fetchData();
-  },[])
+  }, []);
+
   useEffect(() => {
     const fetchToolOptions = async () => {
       try {
@@ -58,73 +61,72 @@ const AddBreakdown = ({ open, handleClose, handleAddBreakdown }) => {
     fetchToolOptions();
     fetchMachineOptions();
   }, []);
-  const [toolss,setToolss] = useState(null)
-  useEffect(()=> {
-    const fetchToolOptionss = async () => {
-      try {
-const response = await axios.get(`${process.env.REACT_APP_URL}/webapp/display-tool-codes/${encodeURIComponent(machineId.label)}`);
-        
-        setToolss(response.data["tool_codes"]);
-      } catch (error) {
-        console.log("Error fetching tool options:", error);
-      }
-    };
-    fetchToolOptionss();
-  },[machineId])
 
-const [filteredToolOptions, setFilteredToolOptions] = useState([]);
-useEffect(() => {
-  const currentDate = new Date().toISOString().split('T')[0];
-  setDate(currentDate);
-}, []);
-
-useEffect(() => {
-  if (toolss) {
-    if (toolCode) {
-      setFilteredToolOptions(toolss.filter(tool => tool !== toolCode.label));
-    } else {
-      setFilteredToolOptions(toolss);
+  useEffect(() => {
+    if (machineId) {
+      const fetchToolOptionss = async () => {
+        try {
+          const response = await axios.get(`${process.env.REACT_APP_URL}/webapp/display-tool-codes/${encodeURIComponent(machineId.label)}`);
+          setToolss(response.data["tool_codes"]);
+        } catch (error) {
+          console.error("Error fetching tool options:", error);
+        }
+      };
+      fetchToolOptionss();
     }
-  }
-}, [toolCode, toolss]);
+  }, [machineId]);
+
+  useEffect(() => {
+    if (toolCode) {
+      const fetchReplacedTools = async () => {
+        try {
+          const response = await axios.get(`${process.env.REACT_APP_URL}/webapp/breakdown/${toolCode.value}/tool-codes/`);
+          setReplacedTools(response.data.tool_codes);
+        } catch (error) {
+          console.error("Error fetching replaced tools:", error);
+        }
+      };
+      fetchReplacedTools();
+    }
+  }, [toolCode]);
+
+  useEffect(() => {
+    if (replacedTools.length > 0 && toolCode) {
+      const updatedFilteredTools = replacedTools.filter(tool => tool !== toolCode.label);
+      setFilteredToolOptions(updatedFilteredTools);
+    }
+  }, [replacedTools, toolCode]);
+
+  useEffect(() => {
+    const currentDate = new Date().toISOString().split('T')[0];
+    setDate(currentDate);
+  }, []);
 
   const handleAdd = () => {
-
-    if (
-      date &&
-      emp_ssn &&
-      toolCode &&
-      machineId &&
-
-      replacedBy &&
-     
-      changeTime &&
-      noOfMinIntoShift
-    ) {
-      console.log(emp_ssn)
+    if (date && empSSN && toolCode && machineId && replacedBy && changeTime && noOfMinIntoShift) {
       const breakdownInfo = {
         date,
-        emp_ssn: emp_ssn.label,
+        emp_ssn: empSSN.label,
         tool_code: toolCode.label,
         machine_id: machineId.label,
         length_used: 10,
         expected_length_remaining: 20,
         replaced_by: replacedBy.label,
-        reason: reason,
+        reason,
         change_time: changeTime,
         no_of_min_into_shift: noOfMinIntoShift,
       };
-      
+
       handleAddBreakdown(breakdownInfo);
       handleClose();
       setDate("");
-    setEmpSSN("");
-    setToolCode("");
-    setMachineId("");
-    setReplacedBy("");
-    setReason("");
-    setChangeTime(0);
-    setNoOfMinIntoShift("");
+      setEmpSSN("");
+      setToolCode("");
+      setMachineId("");
+      setReplacedBy("");
+      setReason("");
+      setChangeTime(0);
+      setNoOfMinIntoShift("");
     } else {
       alert("Please fill in all fields");
     }
@@ -132,17 +134,11 @@ useEffect(() => {
 
   return (
     <div className="w-[400px] px-4">
-    <Dialog open={open} onClose={handleClose} >
-      <DialogTitle>Add Breakdown Information</DialogTitle>
-      <DialogContent>
-        <div>
-
-            <label
-              htmlFor="date"
-              className="block text-lg font-medium text-gray-700"
-            >
-              Date:
-            </label>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Add Breakdown Information</DialogTitle>
+        <DialogContent>
+          <div>
+            <label htmlFor="date" className="block text-lg font-medium text-gray-700">Date:</label>
             <input
               type="date"
               id="date"
@@ -152,118 +148,58 @@ useEffect(() => {
               className="mt-1 block w-[500px] border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-500"
             />
           </div>
-        <div>
-  <label
-    htmlFor="emp_ssn"
-    className="block text-lg font-medium text-gray-700"
-  >
-    Employee SSN:
-  </label>
- <Select
-    options={employeeData?.map(emp => ({ label: emp.emp_ssn, value: emp.emp_ssn }))}
-    value={emp_ssn} // Use the employeeName state variable as the value prop
-    onChange={(selectedOption) => setEmpSSN(selectedOption)} // Update the employeeName state variable with the selected SSN
-    isSearchable
-    placeholder="Select Employee SSN"
-  />
-
-</div>
-
-<div>
-  <label
-    htmlFor="Machine ID"
-    className="block text-lg font-medium text-gray-700"
-  >
-    Machine ID:
-  </label>
- <Select
-    options={machineOptions?.map(machine => ({ label: machine.machine_id, value: machine.machine_id }))}
-    value={machineId} 
-    onChange={(selectedOption) => setMachineId(selectedOption)} // Update the employeeName state variable with the selected SSN
-    isSearchable
-    placeholder="Select Machine ID"
-  />
-
-</div>
-<div>
-  <label
-    htmlFor="Tool Code"
-    className="block text-lg font-medium text-gray-700"
-  >
-    Tool Code:
-  </label>
- <Select
-    options={toolss?.map(tool => ({ label: tool, value: tool }))}
-    value={toolCode} 
-    onChange={(selectedOption) => setToolCode(selectedOption)} // Update the employeeName state variable with the selected SSN
-    isSearchable
-    placeholder="Select Tool Code"
-  />
-
-</div>
-        
-        
-{/*         <TextField */}
-{/*           label="Length Used" */}
-{/*           type="number" */}
-{/*           value={lengthUsed} */}
-{/*           onChange={(e) => setLengthUsed(e.target.value)} */}
-{/*           variant="outlined" */}
-{/*           fullWidth */}
-{/*           size="large" */}
-{/*           margin="normal" */}
-{/*         /> */}
-{/*         <TextField */}
-{/*           label="Expected Length Remaining" */}
-{/*           type="number" */}
-{/*           value={expectedLengthRemaining} */}
-{/*           onChange={(e) => setExpectedLengthRemaining(e.target.value)} */}
-{/*           variant="outlined" */}
-{/*           fullWidth */}
-{/*           size="large" */}
-{/*           margin="normal" */}
-{/*         /> */}
-        <div>
-  <label
-    htmlFor="Replaced By"
-    className="block text-lg font-medium text-gray-700"
-  >
-    Replaced by:
-  </label>
- <Select
-    options={filteredToolOptions?.map(tool => ({ label: tool, value: tool }))}
-    value={replacedBy} 
-    onChange={(selectedOption) => setReplacedBy(selectedOption)} // Update the employeeName state variable with the selected SSN
-    isSearchable
-    placeholder="Replaced By"
-  />
-
-</div>
-        <div>
-
-            <label
-              htmlFor="reason"
-              className="block text-lg font-medium text-gray-700"
-            >
-              Reason:
-            </label>
+          <div>
+            <label htmlFor="emp_ssn" className="block text-lg font-medium text-gray-700">Employee SSN:</label>
+            <Select
+              options={employeeData.map(emp => ({ label: emp.emp_ssn, value: emp.emp_ssn }))}
+              value={empSSN}
+              onChange={(selectedOption) => setEmpSSN(selectedOption)}
+              isSearchable
+              placeholder="Select Employee SSN"
+            />
+          </div>
+          <div>
+            <label htmlFor="machineId" className="block text-lg font-medium text-gray-700">Machine ID:</label>
+            <Select
+              options={machineOptions.map(machine => ({ label: machine.machine_id, value: machine.machine_id }))}
+              value={machineId}
+              onChange={(selectedOption) => setMachineId(selectedOption)}
+              isSearchable
+              placeholder="Select Machine ID"
+            />
+          </div>
+          <div>
+            <label htmlFor="toolCode" className="block text-lg font-medium text-gray-700">Tool Code:</label>
+            <Select
+              options={toolss?.map(tool => ({ label: tool, value: tool }))}
+              value={toolCode}
+              onChange={(selectedOption) => setToolCode(selectedOption)}
+              isSearchable
+              placeholder="Select Tool Code"
+            />
+          </div>
+          <div>
+            <label htmlFor="replacedBy" className="block text-lg font-medium text-gray-700">Replaced by:</label>
+            <Select
+              options={filteredToolOptions.map(tool => ({ label: tool, value: tool }))}
+              value={replacedBy}
+              onChange={(selectedOption) => setReplacedBy(selectedOption)}
+              isSearchable
+              placeholder="Replaced By"
+            />
+          </div>
+          <div>
+            <label htmlFor="reason" className="block text-lg font-medium text-gray-700">Reason:</label>
             <input
               type="text"
               id="reason"
               value={reason}
               onChange={(e) => setReason(e.target.value)}
-              
               className="mt-1 block w-full border-2 py-2 border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-500"
             />
           </div>
-        <div>
-
-            <label
-              htmlFor="number"
-              className="block text-lg font-medium text-gray-700"
-            >
-              Change time:
-            </label>
+          <div>
+            <label htmlFor="changeTime" className="block text-lg font-medium text-gray-700">Change time:</label>
             <input
               type="number"
               id="changeTime"
@@ -273,14 +209,8 @@ useEffect(() => {
               className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-500"
             />
           </div>
-       <div>
-
-            <label
-              htmlFor="number"
-              className="block text-lg font-medium text-gray-700"
-            >
-              Minutes into shift:
-            </label>
+          <div>
+            <label htmlFor="noOfMinIntoShift" className="block text-lg font-medium text-gray-700">Minutes into shift:</label>
             <input
               type="number"
               id="noOfMinIntoShift"
@@ -290,16 +220,12 @@ useEffect(() => {
               className="mt-1 block w-full border-2 py-2 border-gray-300 rounded-md shadow-sm focus:ring focus:ring-indigo-500"
             />
           </div>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose} color="secondary">
-          Cancel
-        </Button>
-        <Button onClick={handleAdd} color="primary">
-          Add
-        </Button>
-      </DialogActions>
-    </Dialog>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="secondary">Cancel</Button>
+          <Button onClick={handleAdd} color="primary">Add</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
